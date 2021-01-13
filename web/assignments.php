6e0bdcdc59e5e2adf0e8410c463bcc28bf47183e
jessicasi <jessicasi@byui.edu>
2021-01-13 11:28:54 -0800

    update assignments

--- web/assignments.php
+++ web/assignments.php
@@ -1,25 +1,5 @@
-<!doctype html>
-<html lang="en">
+<?php include $_SERVER['DOCUMENT_ROOT'] .'/cs313-php/web/common/header.php';
+//phpinfo()?>
 
-<head>
-    <meta charset="utf-8">
-    <title>Assignments</title>
-    <meta name="description" content="Assignments">
-    <meta name="author" content="Jessica Ingebrigtsen">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<h1>Assignments</h1>
+<?php include $_SERVER['DOCUMENT_ROOT'] .'/cs313-php/web/common/footer.php';?>
