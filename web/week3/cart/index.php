d0bc75e0dafa587d3fcc2a398c0d1933f392a765
jessicasi <jessicasi@byui.edu>
2021-01-22 21:06:24 -0800

    test

--- web/week3/cart/index.php
+++ web/week3/cart/index.php
@@ -25,7 +25,7 @@ switch ($action) {
             $_SESSION['cartItems'][] = $itemName;
         }
 
-        include '../shop.php';
+        include '../index.php';
         break;
 
     case 'showCart':
