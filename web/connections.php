011882140101380ff7df01f28f3b572e465992d0
jessicasi <jessicasi@byui.edu>
2021-02-02 13:54:54 -0800

    prepare tests

--- web/connections.php
+++ web/connections.php
@@ -20,5 +20,19 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-echo "test";
+
+foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+
+
 ?>
