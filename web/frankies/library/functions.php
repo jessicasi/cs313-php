73c98e853f2b5484d569511f5b6dd454e4cffa8e
jessicasi <jessicasi@byui.edu>
2021-02-04 21:34:06 -0800

    fix

--- web/frankies/library/functions.php
+++ web/frankies/library/functions.php
@@ -35,8 +35,6 @@ function buildDetailDisplay($animal){
    $dd .= "<section><h3>Description</h3></section>";
    $dd .= "<p>$animal[animal_notes]</p>";
 
-   echo $animal['img_path'];
-   exit;
 
    return $dd;
 }
