52ee104c9de0347659d969bc13b1947f6e3e93aa
jessicasi <jessicasi@byui.edu>
2021-02-05 14:21:59 -0800

    form update

--- web/frankies/view/animals.php
+++ web/frankies/view/animals.php
@@ -1,14 +1,29 @@
-<?php include '../common/header.php'?>
+<?php include '../common/header.php' ?>
 
 <h1 class="classificationHeadings"><?php echo $classification_type; ?></h1>
-<?php if(isset($_SESSION['message'])){
-    echo$_SESSION['message'];}
+<?php if (isset($_SESSION['message'])) {
+    echo $_SESSION['message'];
+}
 ?>
-<?php if(isset($animalDisplay)){
-    echo $animalList;
+
+<div class="formbox">
+    <form method="post" action="/frankies/animals">
+        <label>Filter Results</label>
+        <?php
+        if (!empty($animalList)) {
+            echo $animalList;
+        }
+        ?>
+        <br>
+        <input type="hidden" name="action" value="filterDate">
+        <input type="submit" name="submit" value="Filter">
+    </form>
+</div>
+
+<?php if (isset($animalDisplay)) {
     echo $animalDisplay;
 }
 ?>
 
 
-<?php include '../common/footer.php'?>
+<?php include '../common/footer.php' ?>
