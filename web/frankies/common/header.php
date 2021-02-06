c877adb586a6915f9e8776228b2291f772c79721
jessicasi <jessicasi@byui.edu>
2021-02-05 17:22:34 -0800

    navbar update 10

--- web/frankies/common/header.php
+++ web/frankies/common/header.php
@@ -22,7 +22,7 @@
         <a href='/frankies' title="Frankie's Farm Home Page"><img src="/frankies/images/site/frankieLogo.png" alt="Frankie's Farm Logo" id ="bannerimg"></a>
         <nav class="navbar navbar-expand-md bg-dark navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#"><img src="/frankies/images/site/pig.png" alt="Frankie the Pig"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
