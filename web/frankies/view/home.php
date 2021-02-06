2818086e4031e5df7800a23a7c9a0ae154b45b0d
jessicasi <jessicasi@byui.edu>
2021-02-05 17:58:05 -0800

    image ts

--- web/frankies/view/home.php
+++ web/frankies/view/home.php
@@ -2,7 +2,7 @@
 
 <!-- Background image -->
 <div
-    class="p-5 text-center bg-image"
+    class="p-5 text-center bg-image barn"
   >
     <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
       <div class="d-flex justify-content-center align-items-center h-100">
