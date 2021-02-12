62cae62d83c29763697f3ba335e44fbe5b559a59
jessicasi <jessicasi@byui.edu>
2021-02-11 18:58:30 -0800

    animal admin updates

--- web/frankies/scripts/scripts.js
+++ web/frankies/scripts/scripts.js
@@ -1,11 +1,11 @@
 'use strict' 
  
  // Get a list of vehicles in inventory based on the classificationID 
- let classificationList = document.querySelector("#classificationList"); 
- classificationList.addEventListener("change", function () { 
-  let classificationID = classificationList.value; 
-  console.log(`classificationID is: ${classificationID}`); 
-  let classIDURL = "/phpmotors/vehicles/index.php?action=getInventoryItems&classificationID=" + classificationID; 
+ let typeList = document.querySelector("#typeList"); 
+ typeList.addEventListener("change", function () { 
+  let type_id = typeList.value; 
+  console.log(`type id is: ${type_id}`); 
+  let classIDURL = "/frankies/animals/index.php?action=getAnimalTypes&type_id=" + type_id; 
   fetch(classIDURL) 
   .then(function (response) { 
    if (response.ok) { 
@@ -14,8 +14,7 @@
    throw Error("Network response was not OK"); 
   }) 
   .then(function (data) { 
-   console.log(data); 
-   buildInventoryList(data); 
+   buildAnimalList(data); 
   }) 
   .catch(function (error) { 
    console.log('There was a problem: ', error.message) 
@@ -23,24 +22,23 @@
  })
 
  // Build inventory items into HTML table components and inject into DOM 
-function buildInventoryList(data) { 
-    let inventoryDisplay = document.getElementById("inventoryDisplay"); 
+function buildAnimalList(data) { 
+    let animalDisplay = document.getElementById("animalDisplay"); 
     // Set up the table labels 
     let dataTable = '<thead>'; 
-    dataTable += '<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
+    dataTable += '<tr><th>Animal</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
     dataTable += '</thead>'; 
     // Set up the table body 
     dataTable += '<tbody>'; 
     // Iterate over all vehicles in the array and put each in a row 
     data.forEach(function (element) { 
-     console.log(element.invId + ", " + element.invModel); 
-     dataTable += `<tr><td>${element.invMake} ${element.invModel}</td>`; 
-     dataTable += `<td><a href='/phpmotors/vehicles?action=mod&id=${element.invId}' title='Click to modify'>Modify</a></td>`; 
-     dataTable += `<td><a href='/phpmotors/vehicles?action=del&id=${element.invId}' title='Click to delete'>Delete</a></td></tr>`; 
+     dataTable += `<tr><td>${element.animal_name}</td>`; 
+     dataTable += `<td><a href='/frankies/animals=mod&id=${element.animal_id}' title='Click to modify'>Modify</a></td>`; 
+     dataTable += `<td><a href='/frankies/animals?action=del&id=${element.animal_id}' title='Click to delete'>Delete</a></td></tr>`; 
     }) 
     dataTable += '</tbody>'; 
     // Display the contents in the Vehicle Management view 
-    inventoryDisplay.innerHTML = dataTable; 
+    animalDisplay.innerHTML = dataTable; 
    }
 
 
