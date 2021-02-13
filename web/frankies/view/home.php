30eadb30cd3a9dc1120e25c592be97bf6ea5e1dc
jessicasi <jessicasi@byui.edu>
2021-02-12 18:09:39 -0800

    qSD

--- web/frankies/view/home.php
+++ web/frankies/view/home.php
@@ -1,18 +1,7 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/frankies/common/header.php'; ?>
 
 <!-- Background image -->
-<div
-    class="p-5 text-center bg-image barn"
-  >
-    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
-      <div class="d-flex justify-content-center align-items-center h-100">
-        <div class="text-white">
-          <h1 class="mb-3">Welcome!</h1>
-          <h4 class="mb-3">-</h4>
-        </div>
-      </div>
-    </div>
-  </div>
+
   <!-- Background image -->
 
 <?php include 'common/footer.php'?>
